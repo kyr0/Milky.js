0cf8a2de2e0af9a194933bbd46eb59ce33e12aac
Aron Homberg <info@aron-homberg.de>
2024-11-02 10:45:26 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import * as glob from "glob";
 
 export default {
   root: path.join(__dirname, "."),
+  base: "/geiss.js/",
   build: {
     outDir: path.join(__dirname, "docs"),
     emptyOutDir: true, // also necessary
